ff8ad48637066699a7bbe5d15bc29245e0cca28f
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-09-28 15:31:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/IndiaConstitutionatWork/IndiaConstitutionatWork.pdf';
 
 
 let pdfDoc = null,
